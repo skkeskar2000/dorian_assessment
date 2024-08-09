ce62ce53ea6eac7e8228a906d9afb81dcd030c89
skkeskar2000 <saurabhkeskar2000@gmail.com>
2024-08-09 12:39:56 +0530

    Completed feature

--- main/serializers.py
+++ main/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from main.models import UploadFile, InsuranceData
+from main.models import UploadFile, InsuranceData, Name
 
 
 class UploadFileSerializer(serializers.ModelSerializer):
@@ -8,7 +8,15 @@ class UploadFileSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class NameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Name
+        fields = ['id', 'insurance', 'name', 'clubbed_name']
+
+
 class InsuranceDataSerializer(serializers.ModelSerializer):
+    name = NameSerializer()  # Use the nested serializer for 'name'
+
     class Meta:
         model = InsuranceData
         fields = ['id', 'date', 'name', 'product', 'value']
--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from main.serializers import UploadFileSerializer, InsuranceDataSerializer
 
 
 class InsuranceDataPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
