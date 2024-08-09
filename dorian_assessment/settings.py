f430f88690a296bb39f3d14b4eebe86617af33a3
skkeskar2000 <saurabhkeskar2000@gmail.com>
2024-08-09 13:06:38 +0530

    Completed feature

--- dorian_assessment/settings.py
+++ dorian_assessment/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'dorian_assessment.urls'
